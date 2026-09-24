03a6f071fd5c9106734e0e08e5f5a0c004cc2c0c
”Alireza <alireza.salehfar@tuhh.de>
2026-09-24 14:10:23 +0200

    Initial Commit

---
+++ Proofreader/ProofreaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProofreaderApp.swift
+//  Proofreader
+//
+//  Created by Alireza Salehfar on 24.09.26.
+//
+
+import SwiftUI
+
+@main
+struct ProofreaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
